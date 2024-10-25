d0d3064314c28c1621a4064e791804a6aecc4ff1
rltzhdls <136443821+rltzhdls@users.noreply.github.com>
2024-10-25 15:55:06 +0900

    Add files via upload

---
+++ 434a32.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+    var vds = document.getElementById('vds');
+    
+    if (vds) {
+        vds.src = "https://vod.plaync.com/lineageM/update/221123/oasis2u_bg_05.mp4";
+        document.getElementById('vd').load();
+    }
+});
+
